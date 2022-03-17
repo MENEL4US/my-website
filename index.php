b565827b219cc9003454396120cbb77d62370adf
MENEL4US <luizrodriguesbmm@gmail.com>
2022-03-16 22:47:21 -0300

    layout base

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luiz Mendes</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+    <div class="navigation">
+        <ul>
+            <li>Home</li>
+            <li>Contato</li>
+        </ul>
+    </div>
+
+    <div class="capa">
+
+        <div class="profile-pic">
+            <img src="img/luiz-mendes.jpeg" alt="Foto de Luiz Mendes">
+        </div>
+
+        <div class="profile-title">
+            <h1>Luiz <span>Mendes</span></h1>
+            <h2>Desenvolvedor PHP | Python | Javascript | MySQL</h2>
+
+            
+        </div>
+    </div>
+    
+</body>
+</html>
+<!-- 
+    Luiz Mendes 
+    Florianópolis, SC
+    16/03/2022
+-->
